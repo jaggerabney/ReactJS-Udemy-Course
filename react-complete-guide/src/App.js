67dcb03c3d44a36be31671726b04d86ebb60fed9
Jagger Abney <jagger.abney@gmail.com>
2021-05-03 09:25:22 -0700

    first commit

---
+++ react-complete-guide/src/App.js
@@ -0,0 +1,22 @@
+import './App.css';
+
+function App() {
+  return (
+    // this is called jsx - javascript xml
+    // its a developer-friendly way of writing pages that gets
+    // transformed during compilation
+
+    // pure javascript HTML manipulation is called imperative - 
+    // you are giving clear, step-by-step instructions on what
+    // JS should be doing
+    // it works, but is cumbersome and verbose
+    // with react, we just specify the end state - what we want to see -
+    // and react does all of the heavy lifting in the background
+    <div>
+      <h2>Let's get started!</h2>
+      <p>This is also visible!</p>
+    </div>
+  );
+}
+
+export default App;
