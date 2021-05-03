1dbe08b3b9cd28ce1e721238e312a92507348c21
Jagger Abney <jagger.abney@gmail.com>
2021-05-03 11:15:24 -0700

    extracted calendar component to ExpenseDate file added styling

--- react-complete-guide/src/components/ExpenseItem.js
+++ react-complete-guide/src/components/ExpenseItem.js
@@ -11,22 +11,15 @@
 // css files can be written for components as well;
 // they must be imported for them to work however
 import "./ExpenseItem.css";
+import ExpenseDate from "./ExpenseDate"
 
 function ExpenseItem(props) {
   // data can be passed to components
   // this is done via "props", which are essentially
   // custom HTML attributes
-  const month = props.date.toLocaleString("en-US", { month: "long" });
-  const day = props.date.toLocaleString("en-US", { day: "2-digit" });
-  const year = props.date.getFullYear();
-
   return (
     <div className="expense-item">
-      <div>
-        <div>{month}</div>
-        <div>{day}</div>
-        <div>{year}</div>
-      </div>
+      <ExpenseDate date={props.date}/>
       <div className="expense-item__description">
         <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
