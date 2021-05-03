c7394f003227c819ec085442e5fc28f14f502a09
Jagger Abney <jagger.abney@gmail.com>
2021-05-03 09:49:41 -0700

    added date, title, and amount to ExpenseItem

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,30 +1,30 @@
 import ExpenseItem from "./components/ExpenseItem";
 
 function App() {
-  return (
-    // this is called jsx - javascript xml
-    // its a developer-friendly way of writing pages that gets
-    // transformed during compilation
+    return (
+        // this is called jsx - javascript xml
+        // its a developer-friendly way of writing pages that gets
+        // transformed during compilation
 
-    // pure javascript HTML manipulation is called imperative - 
-    // you are giving clear, step-by-step instructions on what
-    // JS should be doing
-    // it works, but is cumbersome and verbose
-    // with react, we just specify the end state - what we want to see -
-    // and react does all of the heavy lifting in the background
-    
-    // custom components are put into their own files
-    // they exist in a sort of component tree, where the topmost component
-    // is rendered into an html page and all subordinate components
-    // are used as html elements in that page
-    <div>
-      <h2>Let's get started!</h2>
-      <ExpenseItem></ExpenseItem>
-    </div>
+        // pure javascript HTML manipulation is called imperative -
+        // you are giving clear, step-by-step instructions on what
+        // JS should be doing
+        // it works, but is cumbersome and verbose
+        // with react, we just specify the end state - what we want to see -
+        // and react does all of the heavy lifting in the background
 
-    // all react elements must have their first letter capitalized;
-    // that is how they are identified as react components
-  );
+        // custom components are put into their own files
+        // they exist in a sort of component tree, where the topmost component
+        // is rendered into an html page and all subordinate components
+        // are used as html elements in that page
+        <div>
+            <h2>Let's get started!</h2>
+            <ExpenseItem></ExpenseItem>
+        </div>
+
+        // all react elements must have their first letter capitalized;
+        // that is how they are identified as react components
+    );
 }
 
 export default App;
--- react-complete-guide/src/components/ExpenseItem.js
+++ react-complete-guide/src/components/ExpenseItem.js
@@ -1,13 +1,21 @@
-// it's common convention in React applications to 
+// it's common convention in React applications to
 // name things in PascalCase
 
 // components in react are just javascript functions -
 // special ones that return jsx code
 // convention dictates that this function has the same name as the component
 
+// there is one important rule regarding react components:
+// you must have only one root element per component
 function ExpenseItem() {
     return (
-        <h2>Expense item!</h2>
+        <div>
+            <div>May 3rd, 2021</div>
+            <div>
+                <h2>Car Insurance</h2>
+                <div>$294.67</div>
+            </div>
+        </div>
     );
 }
 
