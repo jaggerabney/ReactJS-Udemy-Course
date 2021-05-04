fae34f8ed1928afe0b092f83a4d84f7a3dcc9ff6
Jagger Abney <jagger.abney@gmail.com>
2021-05-04 12:16:22 -0700

    added click handler to ExpenseItem button

--- react-complete-guide/src/components/Expenses/ExpenseItem.js
+++ react-complete-guide/src/components/Expenses/ExpenseItem.js
@@ -3,7 +3,8 @@
 
 // components in react are just javascript functions -
 // special ones that return jsx code
-// convention dictates that this function has the same name as the component
+// convention dictates that
+// this function has the same name as the component
 
 // there is one important rule regarding react components:
 // you must have only one root element per component
@@ -13,20 +14,30 @@
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
+import React, { useState } from "react";
 
 function ExpenseItem(props) {
   // data can be passed to components
   // this is done via "props", which are essentially
   // custom HTML attributes
+
+  // in order to dynamically update aspects of a React component, 
+  // you need to use a React hook - specifically the useState hook
+  // since React apps are only drawn once upon loading, 
+  // there must be something that keeps track of when it should
+  // redraw certain components whose states are being updated
+  // these component instance states exist separately from each other!
+  const [title, setTitle] = useState(props.title);
   function clickHandler() {
-    console.log("Clicked!");
-  };
+    setTitle("Updated!");
+    // console.log(title);
+  }
 
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
       <button onClick={clickHandler}>Change title</button>
