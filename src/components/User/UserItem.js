27cedd986335f2469484cf141fbd1a101e866a3f
Jagger Abney <jagger.abney@gmail.com>
2021-07-10 15:41:00 -0700

    UserItem now reflects proper grammar - fixes #1

--- src/components/User/UserItem.js
+++ src/components/User/UserItem.js
@@ -2,7 +2,7 @@ import "./UserItem.css";
 
 function UserItem(props) {
   return <div className="user-item">
-    <p>{props.username} ({props.age} {props.age === 1 ? "year" : "years"})</p>
+    <p>{props.username} ({props.age} {props.age == 1 ? "year" : "years"})</p>
   </div>
 }
 
