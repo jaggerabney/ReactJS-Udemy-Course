561f4dfe9b82d5a470a5ec57b6f3b009acd2ca9f
Jagger Abney <jagger.abney@gmail.com>
2021-07-10 22:03:31 -0700

    login validity now checked on a 500ms timer

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -12,9 +12,16 @@ const Login = (props) => {
   const [formIsValid, setFormIsValid] = useState(false);
 
   useEffect(() => {
-    setFormIsValid(
-      enteredEmail.includes('@') && enteredPassword.trim().length > 6
-    );
+    const validityCheckTimer = setTimeout(() => {
+      console.log("inside setTimeout");
+      setFormIsValid(
+        enteredEmail.includes('@') && enteredPassword.trim().length > 6
+      );
+    }, 500);
+    return () => {
+      console.log("inside cleanup");
+      clearTimeout(validityCheckTimer);
+    };
   }, [enteredEmail, enteredPassword]);
 
   const emailChangeHandler = (event) => {
